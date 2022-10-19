39dc2874bf7976ff5fb740e11d99850bf28010cc
Elita Chan <elitachan03@gmail.com>
2022-10-18 20:23:47 -0400

    Create function to return results of the round

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function getComputerChoice() {
             break;
     }
 }
+
+function roundResults(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+        else if (computerSelection == "paper") {
+            return "You Lose! Paper beats Rock";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
+    else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You Win! Paper beats Rock";
+        }
+        else if (computerSelection == "scissors"){
+            return "You Lose! Scissors beats Paper";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
+    else {
+        if (computerSelection == "paper") {
+            return "You Win! Scissors beats Paper";
+        }
+        else if (computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
+}
