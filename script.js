b032db709d7f9a574d91bf3697a908ae9ab8d865
Elita Chan <elitachan03@gmail.com>
2022-10-25 15:26:33 -0400

    Revise and condense computer choice and play round

--- script.js
+++ script.js
@@ -1,63 +1,24 @@
 // Global
 let score = 0;
+const rps = ["rock","paper","scissors"];
 
 function getComputerChoice() {
-    const max = 3;
-    const choice = Math.floor(Math.random() * max);
-    switch (choice) {
-        case 0:
-            return "rock";
-            break;
-        case 1:
-            return "paper";
-            break;
-        case 2:
-            return "scissors";
-            break;
-    }
+    return rps[Math.floor(Math.random() * rps.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    if (playerSelection == "rock") {
-        if (computerSelection == "scissors") {
-            score += 1;
-            return "You Win! Rock beats Scissors";
-        }
-        else if (computerSelection == "paper") {
-            score -= 1;
-            return "You Lose! Paper beats Rock";
-        }
-        else {
-            return "It's a tie!";
-        }
+    if (playerSelection === computerSelection) {
+        
     }
-    else if (playerSelection == "paper") {
-        if (computerSelection == "rock") {
-            score += 1;
-            return "You Win! Paper beats Rock";
-        }
-        else if (computerSelection == "scissors"){
-            score -= 1;
-            return "You Lose! Scissors beats Paper";
-        }
-        else {
-            return "It's a tie!";
-        }
+    else if ((playerSelection === "rock" && computerSelection === "scissors") 
+            || (playerSelection === "paper" && computerSelection === "rock")
+            || (playerSelection === "scissors" && computerSelection === "paper")) {
+        score += 1;
     }
     else {
-        if (computerSelection == "paper") {
-            score += 1;
-            return "You Win! Scissors beats Paper";
-        }
-        else if (computerSelection == "rock") {
-            score -= 1;
-            return "You Lose! Rock beats Scissors";
-        }
-        else {
-            return "It's a tie!";
-        }
+        score -= 1;
     }
 }
 
