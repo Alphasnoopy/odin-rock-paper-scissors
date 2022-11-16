ca34b5a462caf0e47f5f45923317cd4c9515a6af
Elita Chan <elitachan03@gmail.com>
2022-11-15 19:55:01 -0500

    Use typewriter effect to type out outputs
    Change cardRules to a nested dictionary holding strengths as key to
    result message value and color as key to corresponding color value.
    
    Push all values for each output to list in sequential order.
    
    Use setTimeOut, Promise, and await/async to set a delay between each
    message. Output message with typewriter effect pkge.

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 // Global
+let Typewriter = window.Typewriter;
 let playerScore = 0;
 let computerScore = 0;
 let roundNum = 0;
 let checkScore = 0;
+let messageList = [];
+let lineNum = 0;
+delayNum = 75;
 const cardHand = document.querySelectorAll('.playerCard');
 const round = document.querySelector('.round');
-const overlay = document.querySelector('.overlay');
-const cardChoices = ["rock","paper","scissors","lizard","spock"];
-const cardRules = {'rock':['scissors', 'lizard'],
-                    'paper':['rock', 'spock'],
-                    'scissors':['paper', 'lizard'],
-                    'lizard':['spock', 'paper'],
-                    'spock':['scissors', 'rock']};
+const cardRules = {'rock': {
+                        'scissors': 'Rock Crushes Scissors!', 
+                        'lizard': 'Rock Crushes Lizard!',
+                        'color': 'gray'},
+                    'paper': {
+                        'rock': 'Paper Covers Rock!', 
+                        'spock': 'Paper Disproves Spock!',
+                        'color': 'bisque'},
+                    'scissors':{
+                        'paper': 'Scissors Cuts Paper!', 
+                        'lizard': 'Scissors Decapitates Lizard!',
+                        'color': 'red'},
+                    'lizard':{
+                        'spock': 'Lizard Poisons Spock!', 
+                        'paper': 'Lizard Eats Paper!',
+                        'color': 'green'},
+                    'spock':{
+                        'scissors': 'Spock Smashes Scissors!', 
+                        'rock': 'Spock Vaporizes Rock!',
+                        'color': 'blue'}};
 const cardFight = new Map(Object.entries(cardRules));
-const compScoreID = document.getElementById('compScore');
-const playerScoreID = document.getElementById('playerScore');
-const battleChoice = document.querySelectorAll('.choice');
 const restartBtn = document.createElement('button');
-const gameBody = document.querySelector('.gameBody');
+const choiceMessage = ["You have chosen ", "Your opponent has chosen "];
+const textLines = document.querySelectorAll('.text');
 
 function roundNumChange() {
     roundNum += 1;
@@ -26,25 +41,48 @@ function roundNumChange() {
 }
 
 function getComputerChoice() {
+    let cardChoices = Object.keys(cardRules);
     return cardChoices[Math.floor(Math.random() * cardChoices.length)];
+}
+
+function choiceColor(colorMatch, choiceMatch) {
+    return `<span style="color: ${colorMatch}">${choiceMatch}</span>`;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-
+        messageList.push('Whew, ', 'It\'s a Tie!');
     }
-    else if (cardFight.get(playerSelection).includes(computerSelection)) {
+    else if (Object.keys(cardFight.get(playerSelection)).includes(computerSelection)) {
         playerScore += 1;
         checkScore += 1;
-        playerScoreID.parentNode.classList.add('scoreAddStart');
-        playerScoreID.parentNode.classList.add('scoreAddEnd');
+        messageList.push('Yay, ', cardFight.get(playerSelection)[computerSelection]);
     }
     else {
         computerScore += 1;
         checkScore += 1;
+        messageList.push('Oh no, ', cardFight.get(computerSelection)[playerSelection]);
     }
-    compScoreID.textContent = computerScore;
-    playerScoreID.textContent = playerScore;
+    messageList.push(`${playerScore} vs ${computerScore}`);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function typeMessage(line, lineMessage) {
+    let typewriter = new Typewriter(line, {delay: delayNum});
+    typewriter
+    .typeString(lineMessage)
+    .start();
+}
+
+async function MessageSeq() {
+    for (const line of textLines) {
+        typeMessage(line, messageList[lineNum]);
+        await sleep(delayNum * (messageList[lineNum].length + 10));
+        lineNum += 1;
+    }
 }
 
 function gameEnd() {
@@ -72,6 +110,14 @@ function game() {
                     choice.classList.add('changeOpacity');
                 }
             });
+            messageList.push(choiceMessage[0], 
+                        choiceColor(cardFight.get(playerSelection).color, playerSelection.toUpperCase()), 
+                        choiceMessage[1], 
+                        choiceColor(cardFight.get(computerSelection).color, computerSelection.toUpperCase()));
+            playRound(playerSelection, computerSelection);
+            const testing = document.querySelectorAll('.testing');
+            lineNum = 0;
+            MessageSeq();
             /*
             roundNumChange();
             playRound(playerSelection.className, computerSelection);
